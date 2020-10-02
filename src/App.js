d413ac8fd86fb7644e949eebe33827cc293a6ae4
makkeess <marcus.jonsson92@live.com>
2020-10-02 15:16:20 +0200

    Remove code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-import Button from 'react-bootstrap/Button';
+//import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hejsan gänget!!!!
-          Lets build this bad boy!
-        </p>
-        <Button variant="primary">Lunch</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
