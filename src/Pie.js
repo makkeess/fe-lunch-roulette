755fe21b9335a0dd1c80b7bc6382c6227da1c091
Marcus Castegren <marcus.castegren@knowit.se>
2020-10-02 16:36:40 +0200

    adding axios to Pie

--- src/Pie.js
+++ src/Pie.js
@@ -2,14 +2,24 @@ import React from "react";
 
 import Slice from "./Slice";
 import { useEffect, useState } from "react";
+import axios from 'axios';
 
 const Pie = () => {
   const [restaurants, setRestaurants] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    setIsLoading(false);
+    console.log(restaurants);
+    axios.get('https://run.mocky.io/v3/f11eea3e-d1da-4bdd-8be6-2f1ad3c517d4').then(resp => {
+      setRestaurants(resp.data);
+      console.log(restaurants);
+      setIsLoading(false);
+    })
   });
-  return <div>{isLoading === true ? <div>laddar</div> : <Slice />}</div>;
+  return <div>
+    {isLoading === true ? <div>laddar</div> : restaurants.map(restaurant => {
+     return <Slice data = {restaurant} />
+    })}
+  </div>;
 };
 
 export default Pie;
